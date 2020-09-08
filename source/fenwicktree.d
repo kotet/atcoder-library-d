09812309d8dacbf468a5816efd5b8f601bf0abec
Kotet <kotet0418@gmail.com>
2020-09-09 08:26:39 +0900

    fenwicktree

---
+++ source/fenwicktree.d
@@ -0,0 +1,64 @@
+module acl.fenwicktree;
+
+unittest
+{
+    auto ft = fenwick_tree!long(100);
+    ft.add(0, 1);
+    ft.add(4, 10);
+    ft.add(9, 100);
+    assert(ft.sum(2, 8) == 10);
+}
+
+// --- fenwicktree ---
+
+struct fenwick_tree(T)
+{
+    import std.traits : isSigned, Unsigned;
+
+    static if (isSigned!T)
+    {
+        alias U = Unsigned!T;
+    }
+    else
+    {
+        alias U = T;
+    }
+public:
+    this(int n)
+    {
+        _n = n;
+        data = new U[](n);
+    }
+
+    void add(int p, T x)
+    {
+        assert(0 <= p && p < _n);
+        p++;
+        while (p <= _n)
+        {
+            data[p - 1] += cast(U) x;
+            p += p & -p;
+        }
+    }
+
+    T sum(int l, int r)
+    {
+        assert(0 <= l && l <= r && r <= _n);
+        return sum(r) - sum(l);
+    }
+
+private:
+    int _n;
+    U[] data;
+
+    U sum(int r)
+    {
+        U s = 0;
+        while (r > 0)
+        {
+            s += data[r - 1];
+            r -= r & -r;
+        }
+        return s;
+    }
+}
