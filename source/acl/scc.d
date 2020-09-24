ef7081914e03792f31b265fd585ff473f9b32c0e
Kotet <kotet0418@gmail.com>
2020-09-24 23:13:04 +0900

    sccにユニットテストを追加

--- source/acl/scc.d
+++ source/acl/scc.d
@@ -2,6 +2,47 @@ module scc;
 
 import acl.internal_scc;
 
+unittest
+{
+    SccGraph graph0;
+    assert([] == graph0.scc());
+    auto graph1 = SccGraph(0);
+    assert([] == graph1.scc());
+}
+
+unittest
+{
+    SccGraph graph;
+    graph = SccGraph(10);
+}
+
+unittest
+{
+    auto graph = SccGraph(2);
+    graph.addEdge(0, 1);
+    graph.addEdge(1, 0);
+    auto scc = graph.scc();
+    assert(1 == scc.length);
+}
+
+unittest
+{
+    auto graph = SccGraph(2);
+    graph.addEdge(0, 0);
+    graph.addEdge(0, 0);
+    graph.addEdge(1, 1);
+    auto scc = graph.scc();
+    assert(2 == scc.length);
+}
+
+unittest
+{
+    import std.exception;
+
+    auto graph = SccGraph(2);
+    assertThrown!Error(graph.addEdge(0, 10));
+}
+
 // --- scc ---
 
 struct SccGraph
